db67866223e4ff21ea7ad36cd3e420f03c8e13b9
root <root@decorous-finger.aeza.network>
2022-10-06 20:46:10 +0300

    abc

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<head>
+<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+
+<body bgcolor="#000080">
+
+
+
+<h1 align="center">
+</p>
+<p style="font-family:'Snell Roundhand', cursive"><font color="yellow" size=250>
+<form action="answer.php" method="post">
+<p> КАК ВАС ЗОВУТ?<input type="text" name="name"/></p>
+<p><input type="submit"/></p>
+</form>
+</font></p>
+
+</body>
+</html>
